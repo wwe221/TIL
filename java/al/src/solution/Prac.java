a794f4ba9fb7ce13531fe6547eaf7b293e587bd1
wwe221 <wwe22123@gmail.com>
2019-06-28 17:09:14 +0900

    Programmers Disc 문제풀다가...

--- java/al/src/solution/Prac.java
+++ java/al/src/solution/Prac.java
@@ -3,32 +3,91 @@ package solution;
 import java.util.*;
 
 public class Prac {
-	public static void main(String[] args) {
-		int stock = 4;
-		int[] dates = { 4, 10, 15 };
-		int[] supp = { 20, 5, 10 };
-		int answer = 0;
-		int l = dates.length;
-		int k = 30;
-		int days = 1;
-		int ind = 0;
-		PriorityQueue<Integer> q = new PriorityQueue<Integer>(Collections.reverseOrder());
-		// 오늘 까지 보급 받을수 있는 것을 내림차순 정렬
-		while (days < k) { // 30일날 까지
-			days++; // 다음날
-//9일째에 밀가루가 바닥나더라도, 10일째에 공급받으면 10일째에는 공장을 운영할 수 있습니다.
-//밀가루가 0이라면,
-			stock--;
-			if (days >= dates[ind] && ind < l - 1) {// 만약 보급날이 지난다면 보급량을 q에 넣는다.
-				q.add(supp[ind]);
-				ind++;
-			}
-			if (stock < 0) { // 쌀이 다 떨어지면 가장 큰 보급품으로 채워 넣는다.
-				System.out.println(days);
-				stock += q.poll();
-				answer++;
-			}
+	public static class number implements Comparable<number> {
+		int getin; // 들어오는 시간
+		int time; // 걸리는 시간
+
+		public number(int getin, int time) {
+			this.getin = getin;
+			this.time = time;
 		}
-		System.out.println(answer);
+
+		public int getGetin() {
+			return getin;
+		}
+
+		public int getTime() {
+			return time;
+		}
+		@Override
+		public int compareTo(number target) {
+	        return this.time >= target.time ? 1 : - 1;
+	    }
+
+		@Override
+		public String toString() {
+			return "number [getin=" + getin + ", time=" + time + "]";
+		}
 	}
+
+	public static void main(String[] args) {
+		int[][] jobs = { { 4, 8 }, { 2, 7 }, { 2, 3 }};
+		int l = jobs.length;
+		PriorityQueue<number> wait = new PriorityQueue<>();
+		// 수행 시간 순으로 정렬한 큐
+		PriorityQueue<number> allq = new PriorityQueue<>(l, new Comparator<number>() {
+			// 요청 시간 순으로 정렬한 큐
+			@Override
+			public int compare(number p1, number p2) {
+				return p1.getin >= p2.getin ? 1 : -1;
+			}
+		});
+		number going = null; // 지금 실행중인 작업.
+		int ttime = 0; // 지나간 시간
+		int answer = 0;
+		int ind = 0;
+		for (int i = 0; i < l; i++) { // 모든 작업들을 요청시간 순으로 정렬 해서 넣는다.
+			wait.offer(new number(jobs[i][0], jobs[i][1]));
+		}
+		System.out.println(wait);
+		
+		// 수행 요청 시간이 지났다면 그것을 수행시간 순으로 wait에 넣는다.
+		while (!allq.isEmpty() || !wait.isEmpty()) {
+			for (int i = 0; i < allq.size(); i++) {
+				if (ttime >= allq.peek().getin) {
+					wait.add(allq.poll());
+					
+				}				
+			}
+			going = wait.poll(); // 다음 순서 녀석을 넣는당.
+			int tmp = going.time + ttime - going.getin;
+			ttime += going.time;
+			answer += tmp;
+			going = null;
+		}
+		System.out.println(wait);
+		System.out.println(answer/3);
+
+// 		while (ind < l) {
+//			for (int i = ind; i < l; i++) {
+//				if (ttime >= jobs[ind][0]) {
+//					wait.add(new number(jobs[ind][0], jobs[ind][1]));
+//					ind++;
+//				}
+//			}
+//			going = wait.poll(); // 다음 순서 녀석을 넣는당.
+//			int tmp = going.time + ttime - going.getin;
+//			ttime += going.time;
+//			answer += tmp;
+//			going = null;
+//
+//		}
+//		System.out.println(wait);
+//		going = wait.poll();
+//		int tmp = going.time + ttime - going.getin;
+//		answer += tmp;
+//		System.out.println(answer / l);
+
+	}
+
 }
