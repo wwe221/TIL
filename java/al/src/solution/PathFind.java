ab1835870e2fee151b514254cfd71e7b84ef487e
wwe221 <wwe22123@gamil.com>
2019-08-12 17:50:37 +0900

    memo

---
+++ java/al/src/solution/PathFind.java
@@ -0,0 +1,42 @@
+package algo;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PathFind {
+	static int[][] answer;
+	static int n;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		answer = new int [n][n];
+		int[][] map = new int[n][n];
+		int [] visit;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				map[i][j] = sc.nextInt();
+			}
+		}
+		for(int i=0;i<n;i++) {
+			visit = new int [n];
+			dfs(map,visit,i);
+		}
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				System.out.print(answer[i][j]);
+			}
+			System.out.println();
+		}
+	}
+
+	public static void dfs(int [][]map , int [] visits , int now) {		
+		for(int i=0;i<n;i++) {
+			if(map[now][i]==1 && visits[i]==0) {
+				visits[i]=1;
+				System.out.println(now + "@@");
+				answer[now][i]=1;
+				dfs(map,visits,i);
+			}
+		}
+	}
+}
